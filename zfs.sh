3af329d8d6dc3d829e547bf99932c732969c748c
serjb <serjb73@yandex.ru>
2023-12-01 19:10:06 +0300

    init commit

---
+++ zfs.sh
@@ -0,0 +1,9 @@
+zpool create otus1 mirror /dev/sdb /dev/sdc
+zpool create otus2 mirror /dev/sdd /dev/sde
+zpool create otus3 mirror /dev/sdf /dev/sdg
+zpool create otus4 mirror /dev/sdh /dev/sdi
+zfs set compression=lzjb otus1
+zfs set compression=lz4 otus2
+zfs set compression=gzip-9 otus3
+zfs set compression=zle otus4
+
